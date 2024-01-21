330424f76b7e98e065b74022c85ab0a572202489
Yoshihiro Sasago <sasayoshi.0605@gmail.com>
2024-01-21 20:38:46 +0900

    Initial Commit

---
+++ Part9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Part9
+//
+//  Created by sasayoshi on 2024/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Part9/Part9App.swift
@@ -0,0 +1,17 @@
+//
+//  Part9App.swift
+//  Part9
+//
+//  Created by sasayoshi on 2024/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Part9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
