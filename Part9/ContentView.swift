888c6a357508f3255d1811d6158aa05e62352dc7
Yoshihiro Sasago <sasayoshi.0605@gmail.com>
2024-01-21 20:51:22 +0900

    AlertとDialogを実装した

--- Part9/ContentView.swift
+++ Part9/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isShowAlert = false
+    @State var isShowDialog = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("アラート"){
+                isShowAlert = true
+            }
         }
         .padding()
+        Button("ダイアログ"){
+            isShowDialog = true
+            
+        }
+        .alert("本当に削除しますか", isPresented: $isShowAlert){
+            Button("削除する",role: .destructive){
+                
+            }
+            Button("キャンセル", role: .cancel){
+                
+            }
+        } message:{
+          Text("一度削除したらもとに戻すことはできません")
+        }
+        .confirmationDialog("タイトル", isPresented: $isShowDialog, titleVisibility: .visible) {
+            Button("選択肢1"){
+                
+            }
+            Button("キャンセル", role: .cancel){
+                
+            }
+        } message: {
+            Text("ここにメッセージ")
+        }
     }
 }
 
